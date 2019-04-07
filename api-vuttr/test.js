bbe44e2f147fe339fe18603d84742437025013a4
Alcindo Schleder <alcindoschleder@gmail.com>
2019-04-07 18:37:58 -0300

    Excluido arquivo test.js

--- api-vuttr/test.js
+++
@@ -1,9 +0,0 @@
-const ApiUtils = require('../commom/utils');
-const env = process.env.NODE_ENV || 'development';
-const config = require('./src/db/config/config.json')[env];
-
-const dsn = ((config.use_env_variable) ?  
-    ApiUtils.genDSN(process.env[config.use_env_variable], 'env') :
-    ApiUtils.genDSN(config, 'config'));
-
-console.log('dsn: ', dsn);
